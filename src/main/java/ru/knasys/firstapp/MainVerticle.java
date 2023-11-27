77f0dbdf6794e19aaf95bfefd03cf7403ef3cdae
Константин Зубрилин <kazubrilin@basistech.ru>
2023-11-27 16:44:37 +1000

    1

--- src/main/java/ru/knasys/firstapp/MainVerticle.java
+++ src/main/java/ru/knasys/firstapp/MainVerticle.java
@@ -1,37 +1,49 @@
 package ru.knasys.firstapp;
 
-import io.vertx.core.*;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
 
-import java.util.concurrent.Callable;
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.time.LocalTime;
 
 public class MainVerticle extends AbstractVerticle {
   public static void main(String[] args) throws Exception {
-    MainVerticle mainVerticle = new MainVerticle();
-    Vertx vertx1 = Vertx.vertx();
-    vertx1.deployVerticle(mainVerticle);
+    //exec1();
+    exec2();
   }
 
-  @Override
-  public void start(Promise<Void> startPromise) throws Exception {
-    int port = 8888;
+  private static void exec1() throws InterruptedException {
+    Promise<String> promise = Promise.promise();
 
-    startPromise.future()
-      .onSuccess(event -> System.out.println("HTTP server started on port " + port))
-      .onFailure(Throwable::printStackTrace)
-      .onFailure(event -> vertx.close());
-
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello world!");
-    }).listen(port, http -> {
-      if (http.succeeded()) {
-        startPromise.complete();
-      } else {
-        startPromise.fail(http.cause());
-      }
+    Vertx.vertx().setTimer(1000, event -> {
+      System.out.println("Complete promice at " + LocalTime.now());
+      promise.complete("result in " + LocalTime.now());
     });
+
+    Thread.sleep(5000L);
+
+    Future<String> future = promise.future();
+
+    System.out.println("Make future on " + LocalTime.now());
+    future.onComplete(event -> System.out.println("Result: " + event));
+  }
+
+  private static void exec2() {
+    Promise<Void> p1 = Promise.<Void>promise();
+    Promise<Void> p2 = Promise.<Void>promise();
+    Promise<Void> p3 = Promise.<Void>promise();
+
+    Future<Void> one = p1.future();
+    Future<Void> two = p2.future();
+    Future<Void> three = p3.future();
+
+    Future.all(one, two, three)
+      .onFailure(event -> System.out.println("!!!!!!!!!!!!!!!!! fail"))
+      .onSuccess(event -> System.out.println("!!!!!!!!!!!!!!!!! success"));
+
+    p1.complete();
+    p2.fail("");
+//    p3.complete();
   }
 }
