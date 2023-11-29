de97dec53ffc33043d23afef904a37258785ab7b
Константин Зубрилин <kazubrilin@basistech.ru>
2023-11-29 16:52:14 +1000

    init

--- src/main/java/ru/knasys/firstapp/MainVerticle.java
+++ src/main/java/ru/knasys/firstapp/MainVerticle.java
@@ -1,14 +1,10 @@
 package ru.knasys.firstapp;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.time.LocalTime;
 
 public class MainVerticle extends AbstractVerticle {
   private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
@@ -17,18 +13,11 @@ public class MainVerticle extends AbstractVerticle {
     Vertx.vertx().createHttpServer()
       .requestHandler(httpServerRequest -> {
         LOG.info("Request to http server: {}", httpServerRequest);
+        httpServerRequest.response()
+          .putHeader("content-type", "text/plain")
+          .end("Hello from Vert.x!");
       })
       .listen(8888)
-      .compose(httpServer -> {
-        LOG.debug("Another task");
-        return Future.succeededFuture(httpServer);
-      })
-      .compose(httpServer -> {
-        LOG.debug("Even more task");
-        Promise<HttpServer> promise = Promise.promise();
-        promise.complete(httpServer);
-        return promise.future();
-      })
       .onFailure(startPromise::fail)
       .onSuccess(httpServer -> {
         LOG.debug("Http server started. Listening on port: {}", httpServer.actualPort());
