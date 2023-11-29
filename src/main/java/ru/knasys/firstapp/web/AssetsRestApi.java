394f043634d5e957df5ebf5204d01332543a0550
Константин Зубрилин <kazubrilin@basistech.ru>
2023-11-29 18:04:48 +1000

    exception handler

--- src/main/java/ru/knasys/firstapp/web/AssetsRestApi.java
+++ src/main/java/ru/knasys/firstapp/web/AssetsRestApi.java
@@ -5,6 +5,7 @@ import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import ru.knasys.firstapp.web.dtos.Asset;
 
 public class AssetsRestApi {
   private static final Logger LOG = LoggerFactory.getLogger(AssetsRestApi.class);
@@ -12,12 +13,19 @@ public class AssetsRestApi {
     parent.get("/assets").handler(context -> {
       JsonArray response = new JsonArray();
       response
-        .add(new JsonObject().put("symbol", "AAPL"))
-        .add(new JsonObject().put("symbol", "AMZN"))
-        .add(new JsonObject().put("symbol", "NFLX"))
-        .add(new JsonObject().put("symbol", "TSLA"));
+        .add(new Asset("AAPL"))
+        .add(new Asset("AMZN"))
+        .add(new Asset("NFLX"))
+        .add(new Asset("TSLA"));
       LOG.info("Path {} responds with {}", context.normalizedPath(), response.encodePrettily());
       context.response().end(response.toBuffer());
+    })
+    .failureHandler(context -> {
+      if (context.response().ended()) return;
+      LOG.error("Router error", context.failure());
+      context.response()
+        .setStatusCode(500)
+        .end(new JsonObject().put("error", context.failure().getMessage()).toBuffer());
     });
   }
 }
---
+++ src/main/java/ru/knasys/firstapp/web/dtos/Asset.java
@@ -0,0 +1,10 @@
+package ru.knasys.firstapp.web.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Asset {
+  private String symbol;
+}
