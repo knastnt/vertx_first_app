c40a802df84be61c6cf6d85f31d9d267679aa711
Константин Зубрилин <kazubrilin@basistech.ru>
2023-11-29 16:28:39 +1000

    start http 8888 server

--- src/main/java/ru/knasys/firstapp/MainVerticle.java
+++ src/main/java/ru/knasys/firstapp/MainVerticle.java
@@ -4,47 +4,35 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.time.LocalTime;
 
 public class MainVerticle extends AbstractVerticle {
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    //exec1();
-    exec2();
-  }
-
-  private static void exec1() throws InterruptedException {
-    Promise<String> promise = Promise.promise();
-
-    Vertx.vertx().setTimer(1000, event -> {
-      System.out.println("Complete promice at " + LocalTime.now());
-      promise.complete("result in " + LocalTime.now());
-    });
-
-    Thread.sleep(5000L);
-
-    Future<String> future = promise.future();
-
-    System.out.println("Make future on " + LocalTime.now());
-    future.onComplete(event -> System.out.println("Result: " + event));
-  }
-
-  private static void exec2() {
-    Promise<Void> p1 = Promise.<Void>promise();
-    Promise<Void> p2 = Promise.<Void>promise();
-    Promise<Void> p3 = Promise.<Void>promise();
-
-    Future<Void> one = p1.future();
-    Future<Void> two = p2.future();
-    Future<Void> three = p3.future();
-
-    Future.join(one, two, three)
-      .onFailure(event -> System.out.println("!!!!!!!!!!!!!!!!! fail"))
-      .onSuccess(event -> System.out.println("!!!!!!!!!!!!!!!!! success"));
-
-    p1.complete();
-    p2.fail("");
-    p3.complete();
+    Vertx.vertx().createHttpServer()
+      .requestHandler(httpServerRequest -> {
+        LOG.info("Request to http server: {}", httpServerRequest);
+      })
+      .listen(8888)
+      .compose(httpServer -> {
+        LOG.debug("Another task");
+        return Future.succeededFuture(httpServer);
+      })
+      .compose(httpServer -> {
+        LOG.debug("Even more task");
+        Promise<HttpServer> promise = Promise.promise();
+        promise.complete(httpServer);
+        return promise.future();
+      })
+      .onFailure(startPromise::fail)
+      .onSuccess(httpServer -> {
+        LOG.debug("Http server started. Listening on port: {}", httpServer.actualPort());
+        startPromise.complete();
+      });
   }
 }
