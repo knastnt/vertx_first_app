415952fa25e95f30c3ecbbaef05aa836689bfbef
Константин Зубрилин <kazubrilin@basistech.ru>
2023-11-30 00:43:04 +1000

    3

--- src/test/java/ru/knasys/firstapp/MainVerticleTest.java
+++ src/test/java/ru/knasys/firstapp/MainVerticleTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(VertxExtension.class)
 class MainVerticleTest {
@@ -24,5 +26,42 @@ class MainVerticleTest {
       testContext.completeNow();
     });
     LOG.debug("End");
+  }
+
+  @Test
+  void test_promiseProcessingInSameThread(Vertx vertx, VertxTestContext testContext) {
+    AtomicReference<String> threadName = new AtomicReference<>();
+    Promise<String> promise = Promise.promise();
+    LOG.debug("Start");
+    vertx.setTimer(2500, id -> {
+      threadName.set(Thread.currentThread().getName());
+      promise.complete();
+      LOG.debug("Complete. Thread: {}", threadName.get());
+      testContext.completeNow();
+    });
+    LOG.debug("End");
+    promise.future()
+      .onSuccess(event -> {
+        LOG.debug("on success future. Thread: {}", Thread.currentThread().getName());
+        assertEquals(threadName.get(), Thread.currentThread().getName());
+      })
+      .onFailure(testContext::failNow);
+  }
+
+  @Test
+  void test_failed(Vertx vertx, VertxTestContext testContext) {
+    Promise<String> promise = Promise.promise();
+    LOG.debug("Start");
+    vertx.setTimer(2500, id -> {
+      promise.future();
+      LOG.debug("Complete.");
+      testContext.completeNow();
+    });
+    LOG.debug("End");
+    promise.future()
+      .onSuccess(testContext::failNow)
+      .onFailure(event -> {
+        LOG.debug("on expected future.");
+      });
   }
 }
