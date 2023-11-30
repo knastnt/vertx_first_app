dd4508ba6e74de0ca958c05b44b6cc18dcf9a9be
knastnt <456kot@mail.ru>
2023-11-30 12:19:24 +1000

    start test

---
+++ src/test/java/ru/knasys/firstapp/web/AssetsRestApiTest.java
@@ -0,0 +1,10 @@
+package ru.knasys.firstapp.web;
+
+import io.vertx.junit5.VertxExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(VertxExtension.class)
+class AssetsRestApiTest {
+
+}
